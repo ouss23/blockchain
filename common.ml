5905a35b14deecdf48e3c4ec9034271d1c8b3552
Oussama Bouzaouit <Oussama.Bouzaouit@u-psud.fr>
2021-01-30 23:31:00 +0100

    common

--- common.ml
+++ common.ml
@@ -1,13 +1,16 @@
 open Unix
+
 type t =
 	| Connect of int 
 	| Message of string
 	| ShowPeers
 
-let ip = inet inet_addr_of_string "127.0.0.1"
+let ip = inet_addr_of_string "127.0.0.1"
+
 let send chan v =
 	output_value chan v;
 	flush chan
+
 let connect_and_send d v =
 	let in_chan, out_chan = open_connection (ADDR_INET(ip, d)) in
 	send out_chan v;
