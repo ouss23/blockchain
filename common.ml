9d26f4af2410ad89195e16f7431a381350719a1c
Nathanb97 <braminathan@yahoo.fr>
2021-01-30 21:05:15 +0100

    common.ml

---
+++ common.ml
@@ -0,0 +1,14 @@
+open Unix
+type t =
+	| Connect of int 
+	| Message of string
+	| ShowPeers
+
+let ip = inet inet_addr_of_string "127.0.0.1"
+let send chan v =
+	output_value chan v;
+	flush chan
+let connect_and_send d v =
+	let in_chan, out_chan = open_connection (ADDR_INET(ip, d)) in
+	send out_chan v;
+	in_chan, out_chan
