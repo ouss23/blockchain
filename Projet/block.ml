ee614828bbac59fdd647c92c0d7c9a62d07b00a0
Oussama Bouzaouit <Oussama.Bouzaouit@u-psud.fr>
2021-02-26 22:49:03 +0100

    verification de la conformité des blocs

--- Projet/block.ml
+++ Projet/block.ml
@@ -1,18 +1,27 @@
 open Transaction
 
 (* type de blocs *)
-type block = { id : int; mutable nonce : int; mutable list_transactions :  transaction list}
+type block = { id : int; prev_hash : string; nonce : int; list_transactions :  transaction list}
 
 (* creer un bloc a partir d'une liste de transactions *)
-let make_block_w_transac id_arg list_transactions_arg = {id=id_arg; nonce = 0; list_transactions=list_transactions_arg}
+let make_block_w_transac id_arg prev_hash_arg list_transactions_arg = {id=id_arg; prev_hash="prev_hash_arg"; nonce = 0; list_transactions=list_transactions_arg}
 
 (* Fonction de verification *)
 let check d difficulty =
- (String.sub d 0 difficulty) = String.make difficulty '\000'
+    (String.sub d 0 difficulty) = String.make difficulty '\000'
+
+(* calculer le hash d'un bloc *)
+let get_hash b =
+    Digest.string (Marshal.to_string b [])
+
+let check_block b difficulty =
+    check (get_hash b) difficulty
 
 (* miner un bloc *)
 let rec puzzle b difficulty =
- let d = Digest.string (Marshal.to_string b []) in 
- if check d difficulty then b
+ if check_block b difficulty then b
  else
   puzzle { b with nonce = b.nonce + 1 } difficulty
+
+let print_block b =
+    Format.printf "Bloc : id = %d, prev_hash = [%s], nonce = %d, transactions count = %d @." b.id b.prev_hash b.nonce (List.length b.list_transactions);
--- Projet/blockchain.ml
+++ Projet/blockchain.ml
@@ -8,16 +8,48 @@ let miningReward = 10
 let mined_blocks = ref []
 
 (* difficulte de minning *)
-let difficulty = 2
+let difficulty = 3
 
 (* liste de transactions en attente de la blockchain *)
 let pending_transactions = ref []
 
 (* nombre de transactions par bloc *)
-let transactions_per_block = 5
+let transactions_per_block = 3
 
 (* l'adresse qui recompense les miners *)
 let miningRewardFromAddress = "block chain"
+
+(* la valeur de prev_hash du premier bloc *)
+let genesis_prev_hash = "0"
+
+(* verifier si une liste de blocs est correcte
+l'enchainement des hashes + les bons nonces *)
+let check_blocks_authenticity b_l =
+    match b_l with
+    (* liste contenant au moins 1 bloc *)
+    | hd :: tl -> 
+        (* pour le genesis, on verifie juste qu'on a le bon nonce *)
+        if (check_block hd difficulty) = false then
+            false
+        (* pour les blocs restants, on verifie l'enchainement des hashes *)
+        else
+            let rec aux b_l2 prev_hash =
+                match b_l2 with
+                (* verifier le nonce & l'enchainement de hashes *)
+                | a :: q -> if ((check_block a difficulty) = false) || ((prev_hash = a.prev_hash) = false) then false
+                            else aux q (get_hash a)
+                (* fin de la liste *)
+                | _ -> true
+            in
+            aux tl (get_hash hd)
+    (* liste vide, on retourne True *)
+    | _ -> true
+
+let last_bloc_hash () =
+    if (List.length !mined_blocks) = 0 then
+        genesis_prev_hash
+    else
+        get_hash (List.hd (List.rev !mined_blocks))
 
 (* Fonction qui mine un block avec la liste des transactions en attente
 On met l'adresse du mineur pour qu'il soit recompense *)
@@ -25,12 +57,12 @@ let minePendingTransactions miningRewardAdress = (
     let first_transactions = List.init transactions_per_block (fun i -> List.nth !pending_transactions i) in
     let remaining_transactions = List.init ((List.length !pending_transactions) - transactions_per_block)
                                             (fun i -> List.nth !pending_transactions (i + transactions_per_block)) in
-    let current_block = make_block_w_transac (List.length !mined_blocks) first_transactions in
+    let current_block = make_block_w_transac (List.length !mined_blocks) (last_bloc_hash ()) first_transactions in
     let mined_block = puzzle current_block difficulty in
-    mined_blocks := mined_block :: !mined_blocks;
+    mined_blocks := !mined_blocks @ [mined_block];
     pending_transactions := (make_transaction miningRewardFromAddress miningRewardAdress miningReward) :: remaining_transactions;
     
-    Format.printf "blocks %d finished with nonce %d@." current_block.id current_block.nonce;
+    Format.printf "Block %d mined with nonce %d@." mined_block.id mined_block.nonce;
 
     pending_transactions := List.init 1 (fun i -> make_transaction miningRewardFromAddress miningRewardAdress miningReward);
     print_transactions !pending_transactions;
@@ -41,7 +73,8 @@ let addTransaction (current_transaction : transaction)= (
     pending_transactions := current_transaction :: !pending_transactions;
     if (List.length !pending_transactions) >= transactions_per_block then
     begin
-        minePendingTransactions "miner"
+        minePendingTransactions "miner";
+        Format.printf "Mined blocks authenticity : %b @." (check_blocks_authenticity !mined_blocks)
     end
 )
 
@@ -49,9 +82,9 @@ let addTransaction (current_transaction : transaction)= (
 let getBalanceOfAddress address = (
   let balance = ref 0 in
   List.iter (fun current_block ->
-    Format.printf "Parse list of block : %d with balance %d @." current_block.id !balance;
+    (* Format.printf "Parse list of block : %d with balance %d @." current_block.id !balance; *)
     List.iter (fun transac ->
-        Format.printf "fromAddress %s  toAddress %s amount %d@." transac.fromAddress transac.toAddress transac.amount;
+        (* Format.printf "fromAddress %s toAddress %s amount %d@." transac.fromAddress transac.toAddress transac.amount; *)
 
         if address = transac.fromAddress then
         begin
@@ -75,3 +108,5 @@ let () =
     addTransaction (make_transaction "from" "to" 10);
     Format.printf "Balance of to %d @." (getBalanceOfAddress "to");
     Format.printf "Balance of from %d @." (getBalanceOfAddress "from");
+    Format.printf "Balance of miner %d @." (getBalanceOfAddress "miner");
+    List.iter (fun block -> print_block block) !mined_blocks;
