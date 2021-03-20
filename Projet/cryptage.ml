481d2257602ed218d2b5582d46a94dc5c4250065
Nathanb97 <braminathan@yahoo.fr>
2021-03-20 21:16:45 +0100

    little clean

--- Projet/cryptage.ml
+++ Projet/cryptage.ml
@@ -1,5 +1,8 @@
 open Unix
-
+(*Fonction pour executer une commande d'un script
+ puis transformer ce que cette commande affiche sur l'output
+ en une liste de chaine de charactere
+ *)
 let process_output_to_list2 = fun command -> 
   let chan = Unix.open_process_in command in
   let res = ref ([] : string list) in
@@ -13,35 +16,32 @@ let process_output_to_list2 = fun command ->
 let cmd_to_list command =
   let (l,_) = process_output_to_list2 command in l
 
-
-
-let print_jsp transactions_list = 
-  (List.iter (fun transac ->
-                  Format.printf "fromAddress %s." transac;
-              ) transactions_list;
-  )
-
-
+(*Creer une clé privé et la retourne sous forme de chaine de charactère*)
 let create_private_key () = (
     let private_key = cmd_to_list "python ../pythonkey/create_private_key.py" in
     String.concat "\n" private_key
   
   )
-
+(*Creer une clé public a partir d'un clé privée et la retourne sous forme de chaine de charactère*)
 let create_public_key private_key = (
   let tmp = "python ../pythonkey/create_public_key.py '" ^ private_key ^"'" in
     let public_key = cmd_to_list  tmp in
     String.concat "\n" public_key 
   
   )
+(*Permet de signer un message et renvoie ce message signé (c'est à dire qu'il est crypter))*)
+let sign_transaction_method private_key message = (
+  let signature = "python ../pythonkey/sign_transaction.py '"  ^ private_key ^ "' '" ^ message ^"'"  in
+  let signature = cmd_to_list  signature in
+  String.concat "\n" signature  
 
-  let sign_transaction_method private_key message = (
-    let signature = "python ../pythonkey/sign_transaction.py '"  ^ private_key ^ "' '" ^ message ^"'"  in
-    let signature = cmd_to_list  signature in
-    String.concat "\n" signature  
-  
-  )
-  let verify_sign_method public_key message signature = (
+)
+
+(*Prend une clé publique, un message, et un message signé (message crypté).
+Renvoie true si le message signé  qui est décrypté avec la clé publique correspond au message.
+Cela permet de verifier si l'autheur du message est bien la même personne que le créteur de la clé publique*)
+
+let verify_sign_method public_key message signature = (
   let cmd = "python ../pythonkey/verify_sign.py --public_key '"  ^ public_key ^ "' --message '" ^ message ^"' --signature '"  ^ signature ^ "'"    in
   let verif = cmd_to_list  cmd in
   let verif = String.concat "\n" verif in
